0abba4f4778af80bce56796c818a2622e5bfa54d
raffialli <raffialli@gmail.com>
2024-02-03 09:32:54 -0500

    deleted hello_raffi.py

--- azure/hello_raffi.py
+++
@@ -1,7 +0,0 @@
-count = 0
-while count < 100:
-    print('I Love Raffi, he is my Mocha Frape with whip cream')
-    count += 1
-print('8=========> ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-# This was not written with ChatGPT, instead it was written with my heart <3
