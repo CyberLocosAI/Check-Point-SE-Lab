9e74acf580fea8dd1be131ad0c7c0934bc1d582d
Travis Lockman <travislockman@gmail.com>
2024-02-05 10:47:18 -0500

    Updating Azure folder

---
+++ azure/core_azure_backbone.tf
@@ -0,0 +1,49 @@
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true #skipping this as a last resort
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "FL-SE-AZURE" {
+  name     = "FL-SE-AZURE-resources"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "FL-SE-AZURE" {
+  count = var.resource_count
+  name  = "FL-SE-AZURE-vnet-${count.index + 1}"
+  #address_space       = ["10.${10 * (count.index + 1)}.0.0/16"]
+  #Adjusted calculation so we don't run into invalid IP space
+  address_space       = ["10.${count.index % 255 + 1}.0.0/16"]
+  location            = azurerm_resource_group.FL-SE-AZURE.location
+  resource_group_name = azurerm_resource_group.FL-SE-AZURE.name
+}
+
+resource "azurerm_subnet" "internal" {
+  count                = var.resource_count
+  name                 = "internal-${count.index + 1}"
+  resource_group_name  = azurerm_resource_group.FL-SE-AZURE.name
+  virtual_network_name = azurerm_virtual_network.FL-SE-AZURE[count.index].name
+  address_prefixes     = ["10.${count.index % 255 + 1}.0.0/24"]
+}
+
+resource "azurerm_subnet" "external" {
+  count                = var.resource_count
+  name                 = "external-${count.index + 1}"
+  resource_group_name  = azurerm_resource_group.FL-SE-AZURE.name
+  virtual_network_name = azurerm_virtual_network.FL-SE-AZURE[count.index].name
+  address_prefixes     = ["10.${count.index % 255 + 1}.1.0/24"]
+}
+
+resource "azurerm_subnet" "dmz" {
+  count                = var.resource_count
+  name                 = "dmz-${count.index + 1}"
+  resource_group_name  = azurerm_resource_group.FL-SE-AZURE.name
+  virtual_network_name = azurerm_virtual_network.FL-SE-AZURE[count.index].name
+  address_prefixes     = ["10.${count.index % 255 + 1}.2.0/24"]
+}
