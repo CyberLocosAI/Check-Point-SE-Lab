8d0f1fa78c8803e9b83be75b691c2d8b14fd77aa
Travis Lockman <travislockman@gmail.com>
2024-02-05 16:13:42 -0500

    deleting archive folder in AWS

--- aws/Archive/output.tf
+++
@@ -1,9 +0,0 @@
-output "ubuntu_instance_public_ips" {
-  value       = aws_instance.ubuntu_server.*.public_ip
-  description = "Public IP addresses of the Ubuntu instances"
-}
-
-output "check_point_manager_public_ips" {
-  value       = aws_instance.vpc_instance.*.public_ip
-  description = "Public IP addresses of the Check Point Manager instances"
-}
