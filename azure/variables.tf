34622e905377cbe66bbe86335c061189ace015d0
raffialli <raffialli@gmail.com>
2024-02-07 20:31:27 -0500

    Refactored variables.tf according to Issues #2

--- azure/variables.tf
+++ azure/variables.tf
@@ -1,18 +1,9 @@
-variable "usernames" {
-  type        = list(string)
-  description = "List of usernames for the virtual machines"
-}
-
 variable "resource_count" {
   description = "The number of each resource type to create"
   type        = number
   default     = 1
 }
 
-variable "passwords" {
-  type        = list(string)
-  description = "List of passwords for the virtual machines"
-}
 variable "subscription_id" {
   description = "The Azure Subscription ID"
   type        = string
@@ -41,6 +32,7 @@ variable "vm_os_sku" {
   description = "The SKU of the Check Point image from Azure Marketplace"
   type        = string
 }
+
 variable "location" {
   description = "The Azure region where the resources will be created"
   type        = string
@@ -59,15 +51,16 @@ variable "vm_size" {
   description = "The size of the virtual machine"
   type        = string
 }
+variable "admin_username" {
+  description = "Admin username for the VDIs"
+  type        = string
+}
+
 variable "admin_password" {
-  description = "The administrator password for the virtual machine"
+  description = "The administrator password for CPManagers"
   type        = string
 }
-variable "bootstrap_script" {
-  description = "Bootstrap script to run on VM initialization"
-  type        = string
-  default     = "" # You can set a default empty string if the script is optional
-}
+
 variable "pub_ips_resource_group_name" {
   description = "The name of the resource group for public IPs"
   type        = string
@@ -78,26 +71,4 @@ variable "ubuntu_monster_vm_size" {
   description = "The size of the Ubuntu Monster virtual machine"
   type        = string
   default     = "Standard_D2s_v3" # You can set a default value or leave it without to require explicit specification
-}
-
-variable "vm_count" {
-  description = "Number of VMs to create for AVD session hosts"
-  type        = number
-  default     = 1
-}
-
-variable "admin_username" {
-  description = "Admin username for the VM"
-  type        = string
-}
-
-variable "student_count" {
-  description = "The number of student VMs to create"
-  type        = number
-  default     = 1
-}
-
-variable "student_subnet_id" {
-  description = "The ID of the subnet where student VMs will be connected"
-  type        = string
 }
